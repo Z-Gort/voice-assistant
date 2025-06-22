064beadc18ec5c2dae40c5598f63b878261f3c91
Zach Gorton <zachgortonnyc@gmail.com>
2025-06-22 16:43:15 -0400

    committing right before moving to eas build

--- backend/src/env.js
+++ backend/src/env.js
@@ -9,6 +9,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     SUPABASE_URL: z.string(),
+    OPENAI_API_KEY: z.string(),
   },
 
   /**
@@ -26,6 +27,8 @@ export const env = createEnv({
    */
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
--- backend/src/server/api/root.ts
+++ backend/src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { otherRouter } from "~/server/api/routers/other";
 import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createCallerFactory, createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  posts: postRouter,
+  other: otherRouter,
 });
 
 // export type definition of API
