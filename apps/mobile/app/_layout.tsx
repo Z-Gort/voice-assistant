7bfa978e916e693b03c5a787186790f2e8218521
Zach Gorton <zachgortonnyc@gmail.com>
2025-06-28 16:08:57 -0400

    livekit room setup maybe working not really tested

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 import { TRPCProvider } from "../lib/TrpcProvider";
+import { registerGlobals } from '@livekit/react-native';
+
+registerGlobals();
 
 export default function RootLayout() {
   return (
--- apps/mobile/app/index.tsx
+++ apps/mobile/app/index.tsx
@@ -1,17 +1,89 @@
-import { Text, View } from "react-native";
+import * as React from 'react';
+import {
+  StyleSheet,
+  View,
+  FlatList,
+  ListRenderItem,
+} from 'react-native';
+import { useEffect } from 'react';
+import {
+  AudioSession,
+  LiveKitRoom,
+  useTracks,
+  TrackReferenceOrPlaceholder,
+  VideoTrack,
+  isTrackReference,
+  registerGlobals,
+} from '@livekit/react-native';
+import { Track } from 'livekit-client';
 
-export default function Index() {
+registerGlobals();
 
+// !! Note !!
+// This sample hardcodes a token which expires in 2 hours.
+const wsURL = "wss://gmailai-v1szfdc6.livekit.cloud"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTExNDEwODEsImlzcyI6IkFQSUdzR3dyZkNiaDVFciIsIm5iZiI6MTc1MTE0MDE4MSwic3ViIjoiYXJ0aWZpY2lhbC13ZWJob29rIiwidmlkZW8iOnsiY2FuVXBkYXRlT3duTWV0YWRhdGEiOnRydWUsInJvb20iOiJzYngtMWF3ZjlmLVNRcEpFeU5idTl5a1d5Z0Y4YkpnSFEiLCJyb29tSm9pbiI6dHJ1ZSwicm9vbUxpc3QiOnRydWV9fQ.tcX-LTPISQvN6HEtAg9bhHZw1TOa9NUytiOgyLU9PqU"
+
+export default function App() {
+  // Start the audio session first.
+  useEffect(() => {
+    let start = async () => {
+      await AudioSession.startAudioSession();
+    };
+
+    start();
+    return () => {
+      AudioSession.stopAudioSession();
+    };
+  }, []);
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+    <LiveKitRoom
+      serverUrl={wsURL}
+      token={token}
+      connect={true}
+      options={{
+        // Use screen pixel density to handle screens with differing densities.
+        adaptiveStream: { pixelDensity: 'screen' },
       }}
+      audio={true}
+      video={true}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <RoomView />
+    </LiveKitRoom>
+  );
+};
+
+const RoomView = () => {
+  // Get all camera tracks.
+  const tracks = useTracks([Track.Source.Camera]);
+
+  const renderTrack: ListRenderItem<TrackReferenceOrPlaceholder> = ({item}) => {
+    // Render using the VideoTrack component.
+    if(isTrackReference(item)) {
+      return (<VideoTrack trackRef={item} style={styles.participantView} />)
+    } else {
+      return (<View style={styles.participantView} />)
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={tracks}
+        renderItem={renderTrack}
+      />
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  participantView: {
+    height: 300,
+  },
+});
