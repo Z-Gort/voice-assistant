a6022d18a8de3d1b2e588acb418e9536457948f9
Zach Gorton <zachgortonnyc@gmail.com>
2025-07-04 15:46:37 -0400

    temp nativewind test

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -2,7 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ['babel-preset-expo', { unstable_transformImportMeta: true }],
+      ["babel-preset-expo", { unstable_transformImportMeta: true }],
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
     ],
   };
 };
